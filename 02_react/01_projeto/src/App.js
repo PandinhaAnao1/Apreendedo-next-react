1644c59f5ace0bdec71359f7786f3ec9e946e9c2
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-10-14 16:10:09 -0400

    Crie o codigo que lista os post de uma pagina

--- 02_react/01_projeto/src/App.js
+++ 02_react/01_projeto/src/App.js
@@ -3,13 +3,40 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  
-  state = { };
 
-  render(){
+  state = {
+    post: [
+      {
+        id: 1,
+        title: 'Titulo do post 1',
+        bory: 'Corpo do post 1'
+      },
+      {
+        id: 2,
+        title: 'Titulo do post 2',
+        bory: 'Corpo do post 2'
+      },
+      {
+        id: 3,
+        title: 'Titulo do post 3',
+        bory: 'Corpo do post 3'
+      }
+    ]
+  };
+
+  render() {
+    const { post } = this.state;
     return (
-      <>
-      </>
+      <div className='App'>
+        {
+          post.map(value => (
+            <div>
+              <h1>{value.title}</h1>
+              <p>{value.bory}</p>
+            </div>
+          ))
+        }
+      </div>
     );
   }
 }
