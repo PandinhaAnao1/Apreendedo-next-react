b0b502735e1362c92b7f198693a4e9aa6e7980d6
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-10-14 17:43:17 -0400

    Coloquei as imagens e corrigir alguns itens

--- 02_react/01_projeto/src/App.js
+++ 02_react/01_projeto/src/App.js
@@ -5,77 +5,66 @@ import { Component } from 'react';
 class App extends Component {
 
   state = {
-    counter: 0,
-    post: [
-      {
-        id: 1,
-        title: 'Titulo do post 1',
-        bory: 'Corpo do post 1'
-      },
-      {
-        id: 2,
-        title: 'Titulo do post 2',
-        bory: 'Corpo do post 2'
-      },
-      {
-        id: 3,
-        title: 'Titulo do post 3',
-        bory: 'Corpo do post 3'
-      }
-    ]
+    post: []
   };
 
   timeOutUpdate = null;
 
-  //Funcão de ciclo de vida que é chamado assim que é carregado o 
-  //compomente
-  componentDidMount(prevProps, prevState, snapshot) {
-    
-    this.handleTimeOut();
-  } 
-  
-  //Parecido com dart esse é o estado quando ele 
-  //atuliza recebe esse itens do estado podemdo 
-  //fazer verificações de como o itens estava estão
-  //e vao estar ao atualizar
-  //Paramentros prevProps, prevState, snapshot
+
+
   componentDidUpdate() {
-    this.handleTimeOut();
   }
 
-  //Funcão de ciclo de vida que é chamada assim que a pagina
-  //Deixa de ser renderizada
+
   componentWillUnmount() {
-    clearTimeout(this.timeOutUpdate);
   }
 
-  handleTimeOut = () => {
-    const { counter, post } = this.state;
-    post[0].title = 'O titulo mudou';
-    this.timeOutUpdate = setTimeout(() => { 
-      this.setState(
-        {
-          post,
-          counter: counter + 1
-        }
-      );
-    }, 1000)
+  componentDidMount() {
+    this.loadPost();
   }
+
+  loadPost = async () => {
+    const postResponse = fetch(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+
+    const photosResponse = fetch(
+      'https://jsonplaceholder.typicode.com/photos'
+    );
+    const [posts,photos] = await Promise.all([postResponse,photosResponse]);
+
+    const postJson = await posts.json();
+
+    const photoJson = await photos.json();
+
+    const postImages = postJson.map((value, index) => (
+      {...value, cover:photoJson[index].url }
+    ));
+
+    this.setState({ post: postImages });
+  }
+
+
 
   render() {
     const { post, counter } = this.state;
     return (
-      <div className='App'>
-        <h1>{counter}</h1>
-        {
-          post.map(value => (
-            <div key={value.id}>
-              <h1>{value.title}</h1>
-              <p>{value.bory}</p>
-            </div>
-          ))
-        }
-      </div>
+      <section className='container'>
+
+        <div className='posts'>
+          {
+            post.map(value => (
+              <div className='post'>
+                <img src={value.cover} alt={value.title}/>
+                <div key={value.id} className='post-content'>
+                  <h1>{value.title}</h1>
+                  <p>{value.body}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </section>
     );
   }
 }
