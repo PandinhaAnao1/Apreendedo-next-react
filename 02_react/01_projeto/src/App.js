619b07382a7c14b5fa4bbc3974ac5974fae36ac8
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-10-14 16:32:04 -0400

    Inserir os ciclos de vida do react

--- 02_react/01_projeto/src/App.js
+++ 02_react/01_projeto/src/App.js
@@ -5,6 +5,7 @@ import { Component } from 'react';
 class App extends Component {
 
   state = {
+    counter: 0,
     post: [
       {
         id: 1,
@@ -24,13 +25,51 @@ class App extends Component {
     ]
   };
 
+  timeOutUpdate = null;
+
+  //Funcão de ciclo de vida que é chamado assim que é carregado o 
+  //compomente
+  componentDidMount(prevProps, prevState, snapshot) {
+    
+    this.handleTimeOut();
+  } 
+  
+  //Parecido com dart esse é o estado quando ele 
+  //atuliza recebe esse itens do estado podemdo 
+  //fazer verificações de como o itens estava estão
+  //e vao estar ao atualizar
+  //Paramentros prevProps, prevState, snapshot
+  componentDidUpdate() {
+    this.handleTimeOut();
+  }
+
+  //Funcão de ciclo de vida que é chamada assim que a pagina
+  //Deixa de ser renderizada
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate);
+  }
+
+  handleTimeOut = () => {
+    const { counter, post } = this.state;
+    post[0].title = 'O titulo mudou';
+    this.timeOutUpdate = setTimeout(() => { 
+      this.setState(
+        {
+          post,
+          counter: counter + 1
+        }
+      );
+    }, 1000)
+  }
+
   render() {
-    const { post } = this.state;
+    const { post, counter } = this.state;
     return (
       <div className='App'>
+        <h1>{counter}</h1>
         {
           post.map(value => (
-            <div>
+            <div key={value.id}>
               <h1>{value.title}</h1>
               <p>{value.bory}</p>
             </div>
