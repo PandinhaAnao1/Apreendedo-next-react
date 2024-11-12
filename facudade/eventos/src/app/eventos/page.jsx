78b2d8829d514b181df6688bac06bc72f67bd334
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-11-11 21:48:51 -0400

    Adicionei o progress indicator para desmonstrar o carregamento da lista

--- facudade/eventos/src/app/eventos/page.jsx
+++ facudade/eventos/src/app/eventos/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import {use} from "react";
+import { use } from "react";
 import { useState, useEffect } from "react";
 
 export default function ListEventos() {
@@ -12,7 +12,7 @@ export default function ListEventos() {
     try {
       setIsLoading(true);
       const response = await fetch(url);
-      if(response.status !== 200) {
+      if (response.status !== 200) {
         throw new Error('Ocorreu um erro ao buscar os eventos');
       }
       const data = await response.json();
@@ -20,7 +20,7 @@ export default function ListEventos() {
     } catch (e) {
       setError(e);
       console.error(e);
-    }finally {
+    } finally {
       setIsLoading(false);
     }
   }
@@ -40,17 +40,17 @@ export default function ListEventos() {
   return (
     <div className="p-4">
       <h1 className="py-5 text-4xl font-bold mb-4 inline-flex">Lista de Eventos</h1>
-      <table className="min-w-full bg-white border border-gray-200">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b text-left">Nome</th>
-            <th className="py-2 px-4 border-b text-left">Data</th>
-            <th className="py-2 px-4 border-b text-left">Ação</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            eventos && eventos.map((evento, index) => (
+      {isLoading === false ?
+
+        <table className="min-w-full bg-white border border-gray-200">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border-b text-left">Nome</th>
+              <th className="py-2 px-4 border-b text-left">Data</th>
+              <th className="py-2 px-4 border-b text-left">Ação</th>
+            </tr>
+          </thead><tbody>
+            {eventos && eventos.map((evento, index) => (
               <tr key={index}>
                 <td className="py-4 px-6 border-b text-left">{evento.titulo}</td>
                 <td className="py-4 px-6 border-b text-left">{formatDate(evento.data)}</td>
@@ -63,10 +63,18 @@ export default function ListEventos() {
                   </button>
                 </td>
               </tr>
-            ))
-          }
-        </tbody>
-      </table>
+            ))}
+          </tbody>
+
+        </table>
+        :
+        <div className="flex justify-center items-center py-4">
+          <svg className="animate-spin h-8 w-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </div>
+      }
     </div>
   );
 }
