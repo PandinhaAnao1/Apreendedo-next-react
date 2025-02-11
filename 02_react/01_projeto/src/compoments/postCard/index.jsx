103ac4281b5e4e3a053f9c5e0caf90c6debe58d4
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2025-02-11 16:28:49 -0400

    fix: inserir uma imagem de alt para caso a image não chege da api

--- 02_react/01_projeto/src/compoments/postCard/index.jsx
+++ 02_react/01_projeto/src/compoments/postCard/index.jsx
@@ -1,11 +1,21 @@
-import './styles.css'
-export const PostCard = ({ cover, title, body }) => (
+import { useState } from 'react';
+import './styles.css';
 
-    <div className='post'>
-        <img src={cover} alt={title} />
-        <div className='post-content'>
-            <h1>{title}</h1>
-            <p>{body}</p>
+export const PostCard = ({ cover, title, body }) => {
+
+    const [imgSrc, setImgSrc] = useState(cover);
+    return (
+
+        <div className='post'>
+            <img
+                src={imgSrc}
+                alt={title || 'Imagem padrão'}
+                onError={() => setImgSrc('/cat-square-1.jpg')}
+            />
+            <div className='post-content'>
+                <h1>{title}</h1>
+                <p>{body}</p>
+            </div>
         </div>
-    </div>
-    )
+    );
+}
