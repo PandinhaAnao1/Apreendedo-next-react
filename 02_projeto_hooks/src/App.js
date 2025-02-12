1859fb365fb5f7d89271af5168794a806acea6f3
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2025-02-12 15:48:29 -0400

    feat: implemente o use memo e o use callback do react

--- 02_projeto_hooks/src/App.js
+++ 02_projeto_hooks/src/App.js
@@ -1,42 +1,34 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import P from 'prop-types';
+import React, { useState, useEffect, use, useCallback } from 'react';
 
-const eventFn = () => {
-  console.log('h1 clicado');
-};
+//Use memo faz cache dos dados da aplicação
+const Button = React.memo(function Button({ icrementButton }){
 
+  return <button onClick={() => icrementButton(10)}>+</button>
+});
+
+Button.prototype = {
+  icrementButton: P.func
+}
+
+// Use callback usado em funções pesadas
+// para poder nao redenrizar novamente os filhos caso as dependencias nao mudem
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  // componentDidUpdate - executa toda vez que o component atualiza
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
-
-  // componentDidMount - executa 1x
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    // componentWillUmount - limpeza
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  }, []);
-
-  // Com dependência - executa toda vez que a dependência mudar
-  useEffect(() => {
-    console.log('C1:', counter, 'C2:', counter2);
-  }, [counter, counter2]);
+  const incrementCounter = useCallback((num) => {
+    //Forma de não criar as dependencias com o counter
+    setCounter((c) =>  c + num);
+  }, [])
 
   return (
     <div className="App">
       <p>Teste 3</p>
       <h1>
-        C1: {counter} C2: {counter2}{' '}
+        C1: {counter}
       </h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter2(counter2 + 1)}>+ (2)</button>
+      <Button icrementButton={incrementCounter}/>
     </div>
   );
 }
